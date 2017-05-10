815435b2c11bd97f58e23ed1ea605db19ba9a6c9
ldzisiuk <luciano.dzisiuk@mercadolibre.com>
2017-05-09 22:12:04 -0300

    integración servicios de búsqueda

--- index.js
+++ index.js
@@ -26,12 +26,103 @@ var ItemModel = require('./src/models/item');
 // Example Route
 var testRouter = express.Router();
 testRouter.get('/', function(req, res) {
-  res.send("Ecologiate!");
+  res.send("<h1>Ecologiate!</h1><br/>Use /api endpoint for requests.");
 });
 app.use(testRouter);
 
 // API routes
 var itemsRouter = express.Router();
+
+itemsRouter.route('/item/:code')
+  .get(function(req, res) {
+    ItemModel.findOne({ code: req.params.code }, function(err, item) {
+      if(!err) {
+        console.log('GET /item/' + req.params.code);
+        if(item){
+          item.statusCode = 200; //200 ok
+          res.send(item);
+        }else{
+          //res.send({'message':'No se encontró un item con el código '+req.params.code});
+          //lo guardo como pending
+          var newItem = new ItemModel({
+            title: 'Item pending '+req.params.code,
+            code: req.params.code,
+            manageable: false,
+            status: 'PENDING',
+            description: 'Item pending '+req.params.code
+          });
+
+          //completo info de apis
+          var options = {
+              host: 'www.googleapis.com',
+              https: true,
+              path: '/customsearch/v1?'+
+                'key=AIzaSyCExeME6nNWBsHRSAlAhGDXQ7UPZXdYu2s'+ //key de mi custom search
+                '&cx=004997307265733943101%3Ayxvqxb5z0g0'+ //id del custom search
+                '&q='+req.params.code, //query del buscador
+              method: 'GET',
+              headers: {
+                  'Content-Type': 'application/json'
+              }
+          };
+
+          var restService = require('./src/services/restService');
+          restService.getJSON(options, function(statusCode, result){
+              // I could work with the result html/json here.  I could also just return it
+              console.log("onResult: (" + statusCode + ")");
+              //actualizo mi item
+              if(result.items != null && result.items.length > 0){
+                var itemSearched = result.items[0];
+                newItem.description = itemSearched.snippet;
+                try{
+                  newItem.img_link = itemSearched.pagemap.cse_thumbnail[0].src;
+                }catch(er){
+                  //nothing
+                }
+                
+                newItem.save(function(err) {
+                  if(!err) {
+                    console.log('Created');
+                  } else {
+                    console.log('ERROR: ' + err);
+                  }
+                });
+              }
+          });
+
+          newItem.save(function(err) {
+            if(!err) {
+              console.log('Created');
+            } else {
+              console.log('ERROR: ' + err);
+            }
+          });
+
+          newItem.statusCode = 201; //created
+          res.send(newItem);
+        }
+      } else {
+        console.log('ERROR: ' + err);
+      }
+    });
+  })
+  .put(function(req, res) {
+    ItemModel.findOne({ 'code': req.params.code }, function(err, item) {
+      item.title   = req.body.title;
+      item.code    = req.body.code;
+      item.number = req.body.number;
+      item.manageable  = req.body.manageable;
+
+      item.save(function(err) {
+        if(!err) {
+          console.log('Updated');
+        } else {
+          console.log('ERROR: ' + err);
+        }
+        res.send(item);
+      });
+    });
+  });
 
 itemsRouter.route('/item')
   .get(function(req, res) {
@@ -64,40 +155,6 @@ itemsRouter.route('/item')
     });
 
     res.send(item);
-  });
-
-itemsRouter.route('/item/:code')
-  .get(function(req, res) {
-    ItemModel.findOne({ 'code': req.params.code }, function(err, item) {
-      if(!err) {
-        console.log('GET /item/' + req.params.code);
-        if(item){
-          res.send(item);
-        }else{
-          var notFoundResponse = {'message':'No se encontró un item con el código '+req.params.code}
-          res.send(notFoundResponse);
-        }
-      } else {
-        console.log('ERROR: ' + err);
-      }
-    });
-  })
-  .put(function(req, res) {
-    ItemModel.findOne({ 'code': req.params.code }, function(err, item) {
-      item.title   = req.body.title;
-      item.code    = req.body.code;
-      item.number = req.body.number;
-      item.manageable  = req.body.manageable;
-
-      item.save(function(err) {
-        if(!err) {
-          console.log('Updated');
-        } else {
-          console.log('ERROR: ' + err);
-        }
-        res.send(item);
-      });
-    });
   });
 
 app.set('port', (process.env.PORT || 5000));
--- src/models/item.js
+++ src/models/item.js
@@ -3,10 +3,12 @@ var mongoose = require('mongoose'); // restful.mongoose;
 
 
 var itemSchema = new mongoose.Schema({
-	title: 		{ type: String },
-	code: 		{ type: String, index: true },
-	number: 	{ type: Number },
-	manageable: { type: Boolean }
+	title:  		{ type: String },
+	code: 			{ type: String, index: true },
+	manageable: 	{ type: Boolean },
+	status: 		{ type: String, index: true},
+	description: 	{ type: String },
+	img_link: 		{ type: String }
 });
 
 
