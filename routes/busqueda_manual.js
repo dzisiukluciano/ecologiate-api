cac314485dff777ba011a3e95149283d4e66b41e
Nico <nicolasmontiel1989@gmail.com>
2017-08-19 19:09:05 -0300

    api de busqueda de producto por nombre

---
+++ routes/busqueda_manual.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+var models  = require('../models');
+
+/* GET items listing. */
+router.get('/', function(req, res, next) {
+  models.producto.findAll().then(function(data){
+    res.send(data)
+  });
+});
+
+router.get('/:codeParam', function(req, res, next) {
+  var codeParametro = req.params.codeParam;
+  models.producto.findOne({ where: {nombre_producto: codeParametro} }).then(producto => {  /*  el code es el de la entidad  */
+    if(producto){
+		console.log("producto encontrado");
+	    producto.status_code = 200; //200 ok
+	    //res.send(producto);
+		models.materiales.findOne({ where: {id: producto.tipo_material} }).then(material => {
+			if(material){
+			  console.log('material encontrado!');
+			  models.categoria.findOne({ where: {id: producto.categoria_id} }).then(categoria => { 
+				 if(material){
+					 console.log('categoria encontrada!');
+					 res.send({producto:producto, material:material, categoria:categoria, status_code:200});
+				 }else{
+				  console.log("material no encontrado");
+				  res.send({status_code:404, mensaje:'Material no encontrado'});
+				}
+			  });
+			}else{
+			  console.log("material no encontrado");
+			  res.send({status_code:404, mensaje:'Material no encontrado'});
+			}
+		});
+    }else{
+      console.log("producto no encontrado");
+      res.send({status_code:404, mensaje:'Producto no encontrado'});
+    }
+  });
+});
+
+
+module.exports = router;
+
+
--- routes/reciclaje_usuario.js
+++ routes/reciclaje_usuario.js
@@ -44,8 +44,8 @@ router.get('/:code/:user/:puntorec/:cant', function(req, res, next) {
 				cantParam = 1;
 			  }
 
-			  //armo respuesta respuesta
-			  var respuesta = new Map();
+			  //armo respuesta 
+			  var respuesta = {};
 			  
 			  console.log(producto.tipo_material.puntos);
 			  console.log(producto.cant_material);
@@ -57,26 +57,23 @@ router.get('/:code/:user/:puntorec/:cant', function(req, res, next) {
 			  respuesta.set('equ_energia',producto.tipo_material.equ_energia * producto.cant_material * cantParam);
 			  respuesta.set('equ_agua',producto.tipo_material.equ_agua * producto.cant_material * cantParam);
 			  */
-			  respuesta.set('puntos_anteriores',user.puntos);
-			  respuesta.set('puntos_sumados', producto.cant_material * cantParam);
-			  respuesta.set('equ_arboles',producto.cant_material * cantParam);
-			  respuesta.set('equ_energia',producto.cant_material * cantParam);
-			  respuesta.set('equ_agua',producto.cant_material * cantParam);
+			  respuesta.puntos_anteriores = user.puntos;
+			  respuesta.puntos_sumados = producto.cant_material * cantParam;
+			  respuesta.equ_arboles = producto.cant_material * cantParam;
+			  respuesta.equ_energia = producto.cant_material * cantParam;
+			  respuesta.equ_agua = producto.cant_material * cantParam;
 
 			  //actualizo los puntos del usuario
 			  
 			  user.updateAttributes({puntos: user.puntos + producto.cant_material * cantParam})
 			  .then((self) => {
-				return self;
+				  console.log(respuesta);
+				  res.send(respuesta);
+					//return self;
 			  }).catch(e => {
 				console.log('Error al actualizar puntos del usuario.\n' + e);
 			  });
 			  //.success(console.log("se actualizaron los puntos del usuario correctamente");
-
-			  //envio respuesta
-			  console.log(respuesta);
-
-			  res.send(respuesta);
 
 			});
 		});
