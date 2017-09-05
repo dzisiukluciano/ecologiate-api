d43b340b41789de18692d925e76e499a6462b315
dzisiukluciano <dzisiukluciano@gmail.com>
2017-09-05 00:25:52 -0300

    busqueda por like y transacciones en reciclar

--- routes/busqueda_manual.js
+++ routes/busqueda_manual.js
@@ -1,42 +1,41 @@
 var express = require('express');
 var router = express.Router();
 var models  = require('../models');
+var sequelize = models.sequelize;
 
-/* GET items listing. */
-router.get('/', function(req, res, next) {
-  models.producto.findAll().then(function(data){
-    res.send(data)
-  });
-});
-
-router.get('/:codeParam', function(req, res, next) {
-  var codeParametro = req.params.codeParam;
-  models.producto.findOne({ where: {nombre_producto: codeParametro} }).then(producto => {  /*  el code es el de la entidad  */
-    if(producto){
-		console.log("producto encontrado");
-	    producto.status_code = 200; //200 ok
-	    //res.send(producto);
-		models.materiales.findOne({ where: {id: producto.tipo_material} }).then(material => {
-			if(material){
-			  console.log('material encontrado!');
-			  models.categoria.findOne({ where: {id: producto.categoria_id} }).then(categoria => { 
-				 if(material){
-					 console.log('categoria encontrada!');
-					 res.send({producto:producto, material:material, categoria:categoria, status_code:200});
-				 }else{
+router.get('/:nombreParam', function(req, res, next) {
+  var nombreParametro = req.params.nombreParam;
+  if(nombreParametro)
+  	nombreParametro = nombreParametro.toLowerCase(); //lo paso a minúscula
+  
+  models.producto.findOne({ 
+  		where: 
+  		sequelize.where(sequelize.fn('LOWER',sequelize.col('nombre_producto')),{$like:'%'+nombreParametro+'%'}) 
+  	})
+  	.then(producto => { 
+	    if(producto){
+			console.log("producto encontrado");
+			models.materiales.findOne({ where: {id: producto.tipo_material} }).then(material => {
+				if(material){
+				  console.log('material encontrado!');
+				  models.categoria.findOne({ where: {id: producto.categoria_id} }).then(categoria => { 
+					 if(categoria){
+						 console.log('categoria encontrada!');
+						 res.send({producto:producto, material:material, categoria:categoria, status_code:200});
+					 }else{
+					  console.log("categoría no encontrada");
+					  res.send({status_code:404, mensaje:'Categoría no encontrada'});
+					}
+				  });
+				}else{
 				  console.log("material no encontrado");
 				  res.send({status_code:404, mensaje:'Material no encontrado'});
 				}
-			  });
-			}else{
-			  console.log("material no encontrado");
-			  res.send({status_code:404, mensaje:'Material no encontrado'});
-			}
-		});
-    }else{
-      console.log("producto no encontrado");
-      res.send({status_code:404, mensaje:'Producto no encontrado'});
-    }
+			});
+	    }else{
+	      console.log("producto no encontrado");
+	      res.send({status_code:404, mensaje:'Producto no encontrado'});
+	    }
   });
 });
 
